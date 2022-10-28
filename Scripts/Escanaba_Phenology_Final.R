317c2914f3bd7759074b7a3f84d4228f3089fa53
zsfeiner <zachary.feiner@wisconsin.gov>
2022-10-28 14:23:56 -0500

    Working pheno predictions with duration

--- Scripts/Escanaba_Phenology_Final.R
+++ Scripts/Escanaba_Phenology_Final.R
@@ -26,22 +26,13 @@ wae <-
   dat %>%
   filter(CorrGear==2, Month %in% c(3:5), Year != 1948) %>%
   group_by(Year, DOY, Date) %>%
-  summarize(Total=n(), Females=sum(Sex=="Female", na.rm=T), PropFemale=Females/Total) %>%
-  group_by(Year) %>%
-  mutate(cumTotal = cumsum(Total), cumFemale=cumsum(Females), propcumTotal=cumTotal/sum(Total), propcumFemale=cumFemale/sum(Females),
-         PropFemaleTotal = Females/sum(Total), scDOY = scale(DOY, center=T, scale=F), fempres = ifelse(Females > 0, 1, 0))
-wae <- wae %>% group_by(Year) %>% mutate(scFem = scale(Females))
-
+  summarize(Total=n(), Females=sum(Sex=="Female", na.rm=T), PropFemale=Females/Total)
 
 ggplot(data=wae, aes(x=DOY, y=Females, group=Year)) + 
   geom_point() + 
   facet_wrap(~Year, scales="free")
 
-ggplot(data=wae[abs(wae$scDOY)<=15,], aes(x=scDOY, y=Females, group=Year)) + 
-  geom_point() + geom_smooth(se=F) +
-  facet_wrap(~Year, scales="free")
-
-ggplot(data=filter(wae, abs(scDOY)<=15), aes(x=DOY, y=Year, group=Year, height=scFem+2,fill=stat(x))) + 
+ggplot(data=wae, aes(x=DOY, y=Year, group=Year, height=Females,fill=stat(x))) + 
   geom_density_ridges_gradient(stat="identity", scale=6) + 
   scale_fill_viridis_c(name="DOY", option="D") + 
   scale_y_continuous(breaks=seq(1944,2040,4)) + theme_bw() + scale_x_continuous(breaks=seq(50,150,5))
@@ -57,22 +48,23 @@ sel.wae <- wae %>%
   filter(Year > 1955) %>%
   group_by(Year) %>%
   filter(abs(mean(DOY) - DOY) < 15) %>%
-  mutate(scDOY = scale(DOY))
+  mutate(scDOY = scale(DOY, center=T, scale=F))
+sel.wae
 
 hist(sel.wae$Females)
 hist(sel.wae$DOY)
 summary(sel.wae$DOY)
 
 #Fit poisson models to check model fit on raw DOY
-modG_pois <- gam(Females ~ s(DOY, bs="cc", k=7) + s(fYear, bs="re"), data=sel.wae, family=poisson(), method="REML", knots=list(DOY=c(80,150)))
-modGS_pois <- gam(Females ~ s(DOY, bs="cc", k=6) + s(DOY, fYear, k=6, bs="fs", xt=list(bs="cc"),m=2) +
-                    s(fYear, bs="re"), data=sel.wae, family=poisson(), method="REML", knots=list(DOY=c(80,150)))
-modGI_pois <- gam(Females ~ s(DOY, bs="cc", k=6) + s(DOY, by=fYear, k=6,  bs="cc",m=2) +
-                    s(fYear, bs="re"), data=sel.wae, family=poisson(), method="REML", knots=list(DOY=c(80,150)))
-
-modG_pois$aic
-modGS_pois$aic
-modGI_pois$aic
+# modG_pois <- gam(Females ~ s(DOY, bs="cc", k=7) + s(fYear, bs="re"), data=sel.wae, family=poisson(), method="REML", knots=list(DOY=c(80,150)))
+# modGS_pois <- gam(Females ~ s(DOY, bs="cc", k=6) + s(DOY, fYear, k=6, bs="fs", xt=list(bs="cc"),m=2) +
+#                     s(fYear, bs="re"), data=sel.wae, family=poisson(), method="REML", knots=list(DOY=c(80,150)))
+# modGI_pois <- gam(Females ~ s(DOY, bs="cc", k=6) + s(DOY, by=fYear, k=6,  bs="cc",m=2) +
+#                     s(fYear, bs="re"), data=sel.wae, family=poisson(), method="REML", knots=list(DOY=c(80,150)))
+# 
+# modG_pois$aic
+# modGS_pois$aic
+# modGI_pois$aic
 
 
 #Fit quasi-poisson models, better fit
@@ -95,16 +87,16 @@ draw(modG)
 draw(modGI)
 
 par(mfrow=c(2,2))
-gam.check(modG_pois)
-gam.check(modGS_pois)
-gam.check(modGI_pois)
+gam.check(modG)
+gam.check(modGS)
+gam.check(modGI)
 
 AIC(modG)
 
 
 lim.pred.data <- sel.wae %>%
   group_by(fYear) %>%
-  summarize(first=min(DOY)-10, last=max(DOY)+10) %>%
+  summarize(first=min(DOY)-20, last=max(DOY)+20) %>%
   group_by(fYear) %>%
   group_modify(~ tibble(DOY = seq(.$first, .$last))) %>%
   ungroup()
@@ -157,3 +149,41 @@ ggplot(data=lim.pred.data, aes(x=DOY, y=modGIfit, group=fYear)) +
   geom_line(aes(x=DOY, y=modGfit, group=fYear), color="red") + 
   geom_line(aes(x=DOY, y=modGSfit, group=fYear), color="green") +
   theme_classic() + facet_wrap(~fYear, scales="free")
+
+
+#Quasi-poisson model GI is the best fit and provides reasonable predictions
+#Extract predictions of day with highest predicted number of spawning females
+#Generate 
+lim.pred.data
+
+esc.phen <- lim.pred.data %>%
+  group_by(fYear) %>%
+  summarize(SpawnDOY = DOY[which.max(modGIfit)])
+
+library(HDInterval)
+
+hdi.year <- tibble()
+for (i in unique(lim.pred.data$fYear)) {
+  set <- filter(lim.pred.data, fYear==i)
+  reps <- lapply(set, rep, set$modGIfit)
+  hdi.year <- bind_rows(hdi.year, HDInterval::hdi(reps$DOY,credMass=0.80))
+}
+hdi.year
+
+esc.phen <- bind_cols(esc.phen, hdi.year) %>%
+  mutate(Duration = upper-lower)
+
+esc.phen
+
+plot(Duration ~ fYear, data=esc.phen)
+plot(Duration ~ SpawnDOY, data=esc.phen)
+
+ggplot(sel.wae, aes(x=DOY, y=Females, group=fYear)) + 
+  geom_point() + 
+  geom_vline(data=esc.phen, aes(xintercept=lower, group=fYear)) + 
+  geom_vline(data=esc.phen, aes(xintercept=upper, group=fYear)) + 
+  facet_wrap(~fYear, scales='free') + 
+  geom_line(data=lim.pred.data, aes(x=DOY, y=modGIfit, group=fYear))
+par(mfrow=c(1,1))
+plot(SpawnDOY ~ as.numeric(fYear), data=esc.phen, type="b")
+
